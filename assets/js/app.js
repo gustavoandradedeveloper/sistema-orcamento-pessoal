ca5dec9ef6863e511ef49a5d71e6925b7d54adf7
Gustavo Andrade <gustavoandradedeveloper@gmail.com>
2022-05-31 11:07:40 -0300

    pegandos os valores adicionado no formulário

---
+++ assets/js/app.js
@@ -0,0 +1,9 @@
+function cadastrarDespesa(){
+    let ano         = document.getElementById('ano').value ;
+    let mes         = document.getElementById('mes').value ;
+    let dia         = document.getElementById('dia').value ;
+    let descricao   = document.getElementById('descricao').value ;
+    let valor       = document.getElementById('valor').value ;
+
+    console.log(mes, dia, descricao,valor)
+}
