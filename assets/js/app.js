8909ff901b65034083007668aaf098927654058b
Gustavo Andrade <gustavoandradedeveloper@gmail.com>
2022-05-31 11:19:28 -0300

    Trabalhando com POO em JS - criando classe despesa

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,32 @@
+class Despesa {
+    constructor(ano,mes,dia,tipo,descricao,valor){
+        this.ano        = ano;
+        this.mes        = mes;
+        this.dia        = dia;
+        this.tipo       = tipo;
+        this.descricao  = descricao;
+        this.valor      = valor;
+    }
+}
+
+
 function cadastrarDespesa(){
     let ano         = document.getElementById('ano').value ;
     let mes         = document.getElementById('mes').value ;
     let dia         = document.getElementById('dia').value ;
+    let tipo        = document.getElementById('tipo').value ;
     let descricao   = document.getElementById('descricao').value ;
     let valor       = document.getElementById('valor').value ;
 
-    console.log(mes, dia, descricao,valor)
+    
+    let objDespesa = new Despesa(
+            ano,
+            mes,
+            dia,
+            tipo,
+            descricao,
+            valor
+        );
+    
+    console.log(objDespesa)
 }
